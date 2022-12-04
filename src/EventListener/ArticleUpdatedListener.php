9abb60b459508a2b05e1727bcdf493bb6f646395
s-damian <contact@damian-freelance.fr>
2022-12-04 16:10:22 +0200

    Update ArticleUpdatedListener

--- src/EventListener/ArticleUpdatedListener.php
+++ src/EventListener/ArticleUpdatedListener.php
@@ -4,10 +4,6 @@ namespace App\EventListener;
 
 use App\Entity\Article;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Event\ExceptionEvent;
-use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
-use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 
 final class ArticleUpdatedListener
 {
