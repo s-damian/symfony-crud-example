7cb0703e01164cbeb3a7aef5ab7f1764c44a496b
s-damian <contact@damian-freelance.fr>
2022-12-04 14:19:16 +0200

    Update ArticleControllerTest

--- tests/Admin/ArticleControllerTest.php
+++ tests/Admin/ArticleControllerTest.php
@@ -9,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class ArticleControllerTest extends WebTestCase
 {
-    public function testIndexClient()
+    public function testIndexClient(): void
     {
         $this->assertTrue(1 === 1);
 
@@ -21,7 +21,7 @@ class ArticleControllerTest extends WebTestCase
         $this->assertSelectorTextContains('h1', 'Article Listing');
     }
 
-    public function testCreateClient()
+    public function testCreateClient(): void
     {
         $this->assertTrue(1 === 1);
 
@@ -31,5 +31,27 @@ class ArticleControllerTest extends WebTestCase
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Create Article');
+    }
+
+    public function testEditClientWith404Error(): void
+    {
+        $this->assertTrue(1 === 1);
+
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/admin/articles/edit/99999');
+
+        $this->assertResponseStatusCodeSame(404, $client->getResponse()->getStatusCode());
+    }
+
+    public function testDeleteClientWith404Error(): void
+    {
+        $this->assertTrue(1 === 1);
+
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/admin/articles/delete/99999');
+
+        $this->assertResponseStatusCodeSame(404, $client->getResponse()->getStatusCode());
     }
 }
